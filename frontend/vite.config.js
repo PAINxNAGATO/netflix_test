e1f2941bf3d25efde3ac2ffc14d27c77a5d7bb0c
Harsh Singh Rawat <99479730+PAINxNAGATO@users.noreply.github.com>
2024-11-05 00:01:49 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  build: {
+    outDir: "build"  // Changes the output folder to 'build'
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true
+      }
+    }
+  }
 });
